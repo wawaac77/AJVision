fbd49d6e9d2aa8667adfb08d6518ff76c0b1aeb0
Alice Jin <alice.c.jin@pccw.com>
2018-02-07 10:56:29 +0800

    AJVision Start

---
+++ AJVision/ImageAnalysis/ImageRecongnitionViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageRecongnitionViewController.h
+//  AJVision
+//
+//  Created by Alice Jin on 7/2/2018.
+//  Copyright © 2018 Alice Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageRecongnitionViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *choosePhotoButton;
+@property (weak, nonatomic) IBOutlet UIButton *recongButton;
+@property (weak, nonatomic) IBOutlet UIView *ansView;
+
+@end
---
+++ AJVision/ImageAnalysis/ImageRecongnitionViewController.m
@@ -0,0 +1,28 @@
+//
+//  ImageRecongnitionViewController.m
+//  AJVision
+//
+//  Created by Alice Jin on 7/2/2018.
+//  Copyright © 2018 Alice Jin. All rights reserved.
+//
+
+#import "ImageRecongnitionViewController.h"
+
+@interface ImageRecongnitionViewController ()
+
+@end
+
+@implementation ImageRecongnitionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+  
+}
+
+
+@end
